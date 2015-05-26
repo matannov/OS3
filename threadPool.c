bd79e2211ddca7f314434bc6064e432a0cd142d4
Matan Nov <matannov@gmail.com>
2015-05-26 19:02:26 +0300

    test things

--- threadPool.c
+++ threadPool.c
@@ -14,11 +14,16 @@
 	This is the `main` routine that manages the thread pool 
 	it waits for tasks to come in and assigns them to threads
 */
+
+static int dead_threads = 0;
+
 void tpThreadRunner(void* pool) {
 	FuncAndParam* func_info = NULL;
 	ThreadPool* tp = (ThreadPool*) pool;
 	while (1) {
+		printf("while 1\n");
 		if (tp->currently_dying) {
+			printf("dead thread %d\n", dead_threads++);
 			return;
 		}
 		if (tp->work_available && !pthread_mutex_lock(&(tp->task_queue_mutex))) {
@@ -29,14 +34,19 @@ void tpThreadRunner(void* pool) {
 			pthread_mutex_unlock(&(tp->task_queue_mutex));
 			pthread_cond_broadcast(&(tp->work_available_cond));
 			func_info->computeFunc(func_info->param);
+			free(func_info);
 		}
-		while ( pthread_mutex_lock(&(tp->work_available_cond_mutex)) ) { }
+		while ( pthread_mutex_lock(&(tp->work_available_cond_mutex)) ) { printf("while 2\n"); }
+		printf("waiting for job 1\n");
 		pthread_cond_wait(&(tp->work_available_cond), &(tp->work_available_cond_mutex));
+		pthread_mutex_unlock(&(tp->work_available_cond_mutex));
+		printf("finish waiting for job \n");
 	}
 }
 
 
 static void tpKiller(ThreadPool* threadPool) {
+	printf("killer instinct\n");
 	pthread_cond_broadcast(&(threadPool->kill_cond));
 }
 
@@ -65,13 +75,17 @@ ThreadPool* tpCreate(int numOfThreads) {
 	tp->threads = (pthread_t*)calloc(numOfThreads, sizeof(pthread_t*));
 	for (i = 0; i < numOfThreads; i++) {
 		err = pthread_create(tp->threads + i, NULL, (void *)tpThreadRunner, tp);
+		printf("made a thread!\n");
 		if (err != 0) { 
+			printf("we're fucked");
 			for (j = 0; j < i; j++) {
 				pthread_cancel(tp->threads[j]);
 			}
 			kill_tp_on_NULL(tp, NULL);
 		}
 	}
+	
+	printf("made tp\n");
 	
 	return tp;
 }
@@ -81,23 +95,29 @@ void tpDestroy(ThreadPool* threadPool, int shouldWaitForTasks) {
 	FuncAndParam* func_info = (FuncAndParam*)calloc(1, sizeof(*func_info));
 	func_info->computeFunc = (void*)tpKiller;
 	func_info->param = threadPool;
-	while (pthread_mutex_lock(&(threadPool->task_queue_mutex))) {   } // must be first to get lock
+	while (pthread_mutex_lock(&(threadPool->task_queue_mutex))) {  printf("while 3\n"); } // must be first to get lock
 	if (!shouldWaitForTasks) {
-		while (osDequeue(threadPool->task_queue)) {   }
+		while (osDequeue(threadPool->task_queue)) { printf("while 4\n");  }
 	}
+	printf("destroy stage1 \n");
+	
 	osEnqueue(threadPool->task_queue, func_info);
 	threadPool->work_available = true;
 	pthread_mutex_unlock(&(threadPool->task_queue_mutex));
 	pthread_cond_broadcast(&(threadPool->work_available_cond));
+	printf("destroy stage2 \n");
 	pthread_mutex_lock(&(threadPool->kill_cond_mutex));
+	printf("waiting 2\n");
 	pthread_cond_wait(&(threadPool->kill_cond), &(threadPool->kill_cond_mutex));
+	printf("finish waiting 2\n");
 	threadPool->currently_dying = true;
-	threadPool->work_available = false;
 	
 	osDestroyQueue(threadPool->task_queue);
 	for (i = 0; i < threadPool->num_threads; i++) {
 		pthread_cond_broadcast(&(threadPool->work_available_cond));
+		printf("joinin1 \n");
 		pthread_join(threadPool->threads[i], NULL);
+		printf("finish join \n");
 	}
 	
 	pthread_mutex_destroy(&(threadPool->dying_tp_mutex));
@@ -107,18 +127,18 @@ void tpDestroy(ThreadPool* threadPool, int shouldWaitForTasks) {
 	pthread_cond_destroy(&(threadPool->work_available_cond));
 	pthread_cond_destroy(&(threadPool->kill_cond));
 	free(threadPool->threads);
-	free(threadPool);	
-	free(func_info);
+	free(threadPool);
+	printf("destroy stage9 \n");
 }
 
 
 int tpInsertTask(ThreadPool* threadPool, void (*computeFunc)(void *),
         void* param) {
 	bool queue_was_empty = false;
-	FuncAndParam* func_info = (FuncAndParam*)calloc(1, sizeof(*func_info)); // FIXME: when to free this??
+	FuncAndParam* func_info = (FuncAndParam*)calloc(1, sizeof(*func_info));
 	func_info->computeFunc = computeFunc;
 	func_info->param = param;
-	while (pthread_mutex_lock(&(threadPool->task_queue_mutex))) { } // FIXME: should not be busy wait
+	while (pthread_mutex_lock(&(threadPool->task_queue_mutex))) { printf("while 5\n"); } // FIXME: should not be busy wait
 	osEnqueue(threadPool->task_queue, func_info);
 	threadPool->work_available = true;
 	pthread_mutex_unlock(&(threadPool->task_queue_mutex));
